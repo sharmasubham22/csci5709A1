854a7511c13439a94db7ef244a68fa9e4d3fe2ef
Subham Sharma <123989894+sharmasubham22@users.noreply.github.com>
2023-02-06 21:51:21 -0400

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import '../App.css';
 import React  from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer() {
 
@@ -7,12 +8,12 @@ function Footer() {
         <>
         <footer>
   <p>DigiSoul: A Tech World</p>
-  <p><a className="foot-link" href="/">Blogs</a> | 
-  <a className="foot-link" href="/"> Phones</a> | 
-  <a className="foot-link" href="/"> Laptops</a> | 
-  <a className="foot-link" href="/"> Accessories</a> | 
-  <a className="foot-link" href="/"> Reviews</a> | 
-  <a className="foot-link" href="/"> Events</a></p>
+  <p><Link className="foot-link" to="/">Blogs</Link> | 
+  <Link className="foot-link" to="/"> Phones</Link> | 
+  <Link className="foot-link" to="/"> Laptops</Link> | 
+  <Link className="foot-link" to="/"> Accessories</Link> | 
+  <Link className="foot-link" to="/"> Reviews</Link> | 
+  <Link className="foot-link" to="/"> Events</Link></p>
   
 </footer>
         </>
